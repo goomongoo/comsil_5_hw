c6af1e355509e201f38703814c159dcbaf6a3238
goomongoo <mongoo8947@naver.com>
2024-04-15 14:45:03 +0900

    first branch

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+void first_branch() {
+    printf("Hello from first branch!\n");
+}
+
 int main(void) {
     printf("Hello from main branch!\n");
+    first_branch();
 
     return 0;
 }
