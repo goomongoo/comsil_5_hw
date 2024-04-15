de540ec7e58da9a8216995d33cfb77c48a5a3373
goomongoo <mongoo8947@naver.com>
2024-04-15 14:47:53 +0900

    second branch

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+void second_branch() {
+    printf("Hello from second branch!\n");
+}
+
 int main(void) {
     printf("Hello from main branch!\n");
+    second_branch();
 
     return 0;
 }
